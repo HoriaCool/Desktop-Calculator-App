d41a81941bd4bc4c021a8b3eaba181ab4acb34f0
FacCeVreauDin99 <nedelcu.horia.alexandru@gmail.com>
2019-10-23 15:16:33 +0300

    project push

---
+++ Calcul/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace Calcul
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Afisare = new System.Windows.Forms.TextBox();
+            this.Cifra1 = new System.Windows.Forms.Button();
+            this.Cifra2 = new System.Windows.Forms.Button();
+            this.Cifra3 = new System.Windows.Forms.Button();
+            this.Cifra0 = new System.Windows.Forms.Button();
+            this.Plus = new System.Windows.Forms.Button();
+            this.Egal = new System.Windows.Forms.Button();
+            this.Minus = new System.Windows.Forms.Button();
+            this.Ori = new System.Windows.Forms.Button();
+            this.Div = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.Cifra4 = new System.Windows.Forms.Button();
+            this.Cifra5 = new System.Windows.Forms.Button();
+            this.Cifra6 = new System.Windows.Forms.Button();
+            this.Cifra7 = new System.Windows.Forms.Button();
+            this.Cifra8 = new System.Windows.Forms.Button();
+            this.Cifra9 = new System.Windows.Forms.Button();
+            this.Opusul = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Afisare
+            // 
+            this.Afisare.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.Afisare.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Afisare.Location = new System.Drawing.Point(12, 12);
+            this.Afisare.Name = "Afisare";
+            this.Afisare.Size = new System.Drawing.Size(568, 38);
+            this.Afisare.TabIndex = 0;
+            this.Afisare.TextChanged += new System.EventHandler(this.Afisare_TextChanged);
+            // 
+            // Cifra1
+            // 
+            this.Cifra1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Cifra1.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cifra1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Cifra1.Location = new System.Drawing.Point(25, 79);
+            this.Cifra1.Name = "Cifra1";
+            this.Cifra1.Size = new System.Drawing.Size(70, 62);
+            this.Cifra1.TabIndex = 1;
+            this.Cifra1.Text = "1";
+            this.Cifra1.UseVisualStyleBackColor = false;
+            this.Cifra1.Click += new System.EventHandler(this.Cifra1_Click);
+            // 
+            // Cifra2
+            // 
+            this.Cifra2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Cifra2.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.Cifra2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Cifra2.Location = new System.Drawing.Point(115, 78);
+            this.Cifra2.Name = "Cifra2";
+            this.Cifra2.Size = new System.Drawing.Size(70, 62);
+            this.Cifra2.TabIndex = 2;
+            this.Cifra2.Text = "2";
+            this.Cifra2.UseVisualStyleBackColor = false;
+            this.Cifra2.Click += new System.EventHandler(this.Cifra2_Click);
+            // 
+            // Cifra3
+            // 
+            this.Cifra3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Cifra3.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.Cifra3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Cifra3.Location = new System.Drawing.Point(209, 78);
+            this.Cifra3.Name = "Cifra3";
+            this.Cifra3.Size = new System.Drawing.Size(70, 62);
+            this.Cifra3.TabIndex = 3;
+            this.Cifra3.Text = "3";
+            this.Cifra3.UseVisualStyleBackColor = false;
+            this.Cifra3.Click += new System.EventHandler(this.Cifra3_Click);
+            // 
+            // Cifra0
+            // 
+            this.Cifra0.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Cifra0.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.Cifra0.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Cifra0.Location = new System.Drawing.Point(304, 250);
+            this.Cifra0.Name = "Cifra0";
+            this.Cifra0.Size = new System.Drawing.Size(70, 62);
+            this.Cifra0.TabIndex = 4;
+            this.Cifra0.Text = "0";
+            this.Cifra0.UseVisualStyleBackColor = false;
+            this.Cifra0.Click += new System.EventHandler(this.Cifra0_Click);
+            // 
+            // Plus
+            // 
+            this.Plus.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Plus.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.Plus.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Plus.Location = new System.Drawing.Point(304, 80);
+            this.Plus.Name = "Plus";
+            this.Plus.Size = new System.Drawing.Size(70, 61);
+            this.Plus.TabIndex = 5;
+            this.Plus.Text = "+";
+            this.Plus.UseVisualStyleBackColor = false;
+            this.Plus.Click += new System.EventHandler(this.Plus_Click);
+            // 
+            // Egal
+            // 
+            this.Egal.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Egal.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.Egal.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Egal.Location = new System.Drawing.Point(402, 250);
+            this.Egal.Name = "Egal";
+            this.Egal.Size = new System.Drawing.Size(70, 62);
+            this.Egal.TabIndex = 6;
+            this.Egal.Text = "=";
+            this.Egal.UseVisualStyleBackColor = false;
+            this.Egal.Click += new System.EventHandler(this.Egal_Click);
+            // 
+            // Minus
+            // 
+            this.Minus.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Minus.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.Minus.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Minus.Location = new System.Drawing.Point(402, 80);
+            this.Minus.Name = "Minus";
+            this.Minus.Size = new System.Drawing.Size(70, 62);
+            this.Minus.TabIndex = 7;
+            this.Minus.Text = "-";
+            this.Minus.UseVisualStyleBackColor = false;
+            this.Minus.Click += new System.EventHandler(this.Minus_Click);
+            // 
+            // Ori
+            // 
+            this.Ori.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Ori.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.Ori.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Ori.Location = new System.Drawing.Point(304, 163);
+            this.Ori.Name = "Ori";
+            this.Ori.Size = new System.Drawing.Size(70, 62);
+            this.Ori.TabIndex = 8;
+            this.Ori.Text = "*";
+            this.Ori.UseVisualStyleBackColor = false;
+            this.Ori.Click += new System.EventHandler(this.Ori_Click);
+            // 
+            // Div
+            // 
+            this.Div.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Div.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.Div.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Div.Location = new System.Drawing.Point(402, 163);
+            this.Div.Name = "Div";
+            this.Div.Size = new System.Drawing.Size(70, 62);
+            this.Div.TabIndex = 9;
+            this.Div.Text = "/";
+            this.Div.UseVisualStyleBackColor = false;
+            this.Div.Click += new System.EventHandler(this.Div_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Reset.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.Reset.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Reset.Location = new System.Drawing.Point(511, 175);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(57, 112);
+            this.Reset.TabIndex = 10;
+            this.Reset.Text = "C";
+            this.Reset.UseVisualStyleBackColor = false;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Cifra4
+            // 
+            this.Cifra4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Cifra4.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cifra4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Cifra4.Location = new System.Drawing.Point(25, 163);
+            this.Cifra4.Name = "Cifra4";
+            this.Cifra4.Size = new System.Drawing.Size(70, 62);
+            this.Cifra4.TabIndex = 11;
+            this.Cifra4.Text = "4";
+            this.Cifra4.UseVisualStyleBackColor = false;
+            this.Cifra4.Click += new System.EventHandler(this.Cifra4_Click);
+            // 
+            // Cifra5
+            // 
+            this.Cifra5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Cifra5.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.Cifra5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Cifra5.Location = new System.Drawing.Point(115, 163);
+            this.Cifra5.Name = "Cifra5";
+            this.Cifra5.Size = new System.Drawing.Size(70, 62);
+            this.Cifra5.TabIndex = 12;
+            this.Cifra5.Text = "5";
+            this.Cifra5.UseVisualStyleBackColor = false;
+            this.Cifra5.Click += new System.EventHandler(this.Cifra5_Click);
+            // 
+            // Cifra6
+            // 
+            this.Cifra6.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Cifra6.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.Cifra6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Cifra6.Location = new System.Drawing.Point(209, 163);
+            this.Cifra6.Name = "Cifra6";
+            this.Cifra6.Size = new System.Drawing.Size(70, 62);
+            this.Cifra6.TabIndex = 13;
+            this.Cifra6.Text = "6";
+            this.Cifra6.UseVisualStyleBackColor = false;
+            this.Cifra6.Click += new System.EventHandler(this.Cifra6_Click);
+            // 
+            // Cifra7
+            // 
+            this.Cifra7.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Cifra7.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.Cifra7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Cifra7.Location = new System.Drawing.Point(25, 250);
+            this.Cifra7.Name = "Cifra7";
+            this.Cifra7.Size = new System.Drawing.Size(70, 62);
+            this.Cifra7.TabIndex = 14;
+            this.Cifra7.Text = "7";
+            this.Cifra7.UseVisualStyleBackColor = false;
+            this.Cifra7.Click += new System.EventHandler(this.Cifra7_Click);
+            // 
+            // Cifra8
+            // 
+            this.Cifra8.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Cifra8.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.Cifra8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Cifra8.Location = new System.Drawing.Point(115, 250);
+            this.Cifra8.Name = "Cifra8";
+            this.Cifra8.Size = new System.Drawing.Size(70, 62);
+            this.Cifra8.TabIndex = 15;
+            this.Cifra8.Text = "8";
+            this.Cifra8.UseVisualStyleBackColor = false;
+            this.Cifra8.Click += new System.EventHandler(this.Cifra8_Click);
+            // 
+            // Cifra9
+            // 
+            this.Cifra9.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Cifra9.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.Cifra9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Cifra9.Location = new System.Drawing.Point(209, 250);
+            this.Cifra9.Name = "Cifra9";
+            this.Cifra9.Size = new System.Drawing.Size(70, 62);
+            this.Cifra9.TabIndex = 16;
+            this.Cifra9.Text = "9";
+            this.Cifra9.UseVisualStyleBackColor = false;
+            this.Cifra9.Click += new System.EventHandler(this.Cifra9_Click);
+            // 
+            // Opusul
+            // 
+            this.Opusul.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Opusul.Font = new System.Drawing.Font("Modern No. 20", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.Opusul.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Opusul.Location = new System.Drawing.Point(505, 80);
+            this.Opusul.Name = "Opusul";
+            this.Opusul.Size = new System.Drawing.Size(63, 60);
+            this.Opusul.TabIndex = 17;
+            this.Opusul.Text = "+/-";
+            this.Opusul.UseVisualStyleBackColor = false;
+            this.Opusul.Click += new System.EventHandler(this.Opusul_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(592, 324);
+            this.Controls.Add(this.Opusul);
+            this.Controls.Add(this.Cifra9);
+            this.Controls.Add(this.Cifra8);
+            this.Controls.Add(this.Cifra7);
+            this.Controls.Add(this.Cifra6);
+            this.Controls.Add(this.Cifra5);
+            this.Controls.Add(this.Cifra4);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Div);
+            this.Controls.Add(this.Ori);
+            this.Controls.Add(this.Minus);
+            this.Controls.Add(this.Egal);
+            this.Controls.Add(this.Plus);
+            this.Controls.Add(this.Cifra0);
+            this.Controls.Add(this.Cifra3);
+            this.Controls.Add(this.Cifra2);
+            this.Controls.Add(this.Cifra1);
+            this.Controls.Add(this.Afisare);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Afisare;
+        private System.Windows.Forms.Button Cifra1;
+        private System.Windows.Forms.Button Cifra2;
+        private System.Windows.Forms.Button Cifra3;
+        private System.Windows.Forms.Button Cifra0;
+        private System.Windows.Forms.Button Plus;
+        private System.Windows.Forms.Button Egal;
+        private System.Windows.Forms.Button Minus;
+        private System.Windows.Forms.Button Ori;
+        private System.Windows.Forms.Button Div;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button Cifra4;
+        private System.Windows.Forms.Button Cifra5;
+        private System.Windows.Forms.Button Cifra6;
+        private System.Windows.Forms.Button Cifra7;
+        private System.Windows.Forms.Button Cifra8;
+        private System.Windows.Forms.Button Cifra9;
+        private System.Windows.Forms.Button Opusul;
+    }
+}
+
---
+++ Calcul/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calcul
+{
+
+    public partial class Form1 : Form
+    {
+        char c = '+';
+        double val = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Afisare_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Cifra0_Click(object sender, EventArgs e)
+        {
+            if (Afisare.Text != "0")
+                Afisare.Text += '0';
+        }
+
+        private void Cifra1_Click(object sender, EventArgs e)
+        {
+            if (Afisare.Text != "0")
+                Afisare.Text += '1';
+        }
+
+        private void Cifra2_Click(object sender, EventArgs e)
+        {
+            if (Afisare.Text != "0")
+                Afisare.Text += '2';
+        }
+
+        private void Cifra3_Click(object sender, EventArgs e)
+        {
+            if (Afisare.Text != "0")
+                Afisare.Text += '3';
+        }
+
+        private void Cifra4_Click(object sender, EventArgs e)
+        {
+            if (Afisare.Text != "0")
+                Afisare.Text += '4';
+        }
+
+        private void Cifra5_Click(object sender, EventArgs e)
+        {
+            if (Afisare.Text != "0")
+                Afisare.Text += '5';
+        }
+
+        private void Cifra6_Click(object sender, EventArgs e)
+        {
+            if (Afisare.Text != "0")
+                Afisare.Text += '6';
+        }
+
+        private void Cifra7_Click(object sender, EventArgs e)
+        {
+            if (Afisare.Text != "0")
+                Afisare.Text += '7';
+        }
+
+        private void Cifra8_Click(object sender, EventArgs e)
+        {
+            if (Afisare.Text != "0")
+                Afisare.Text += '8';
+        }
+
+        private void Cifra9_Click(object sender, EventArgs e)
+        {
+            if (Afisare.Text != "0")
+                Afisare.Text += '9';
+        }
+
+        private void Plus_Click(object sender, EventArgs e)
+        {
+            if (Afisare.Text != null)
+            {
+                if (c == '+')
+                    val = val + double.Parse(Afisare.Text);
+                if (c == '-')
+                    val = val - double.Parse(Afisare.Text);
+                if (c == '*')
+                    val = val * double.Parse(Afisare.Text);
+                if (c == '/')
+                    val = val / double.Parse(Afisare.Text);
+            }
+
+            c = '+';
+            Afisare.Text = null;
+        }
+
+        private void Minus_Click(object sender, EventArgs e)
+        {
+            if (Afisare.Text != null)
+            {
+                if (c == '+')
+                    val = val + double.Parse(Afisare.Text);
+                if (c == '-')
+                    val = val - double.Parse(Afisare.Text);
+                if (c == '*')
+                    val = val * double.Parse(Afisare.Text);
+                if (c == '/')
+                    val = val / double.Parse(Afisare.Text);
+            }
+
+            c = '-';
+            Afisare.Text = null;
+        }
+
+        private void Ori_Click(object sender, EventArgs e)
+        { 
+            if (Afisare.Text != null)
+            {
+                if (c == '+')
+                    val = val + double.Parse(Afisare.Text);
+                if (c == '-')
+                    val = val - double.Parse(Afisare.Text);
+                if (c == '*')
+                    val = val * double.Parse(Afisare.Text);
+                if (c == '/')
+                    val = val / double.Parse(Afisare.Text);
+            }
+
+            c = '*';
+            Afisare.Text = null;
+        }
+
+        private void Div_Click(object sender, EventArgs e)
+        {
+            if (Afisare.Text != null)
+            {
+                if (c == '+')
+                    val = val + double.Parse(Afisare.Text);
+                if (c == '-')
+                    val = val - double.Parse(Afisare.Text);
+                if (c == '*')
+                    val = val * double.Parse(Afisare.Text);
+                if (c == '/')
+                    val = val / double.Parse(Afisare.Text);
+            }
+            c = '/';
+            Afisare.Text = null;
+        }
+
+        private void Egal_Click(object sender, EventArgs e)
+        {
+            if (c == ' ')
+            {
+                Afisare.Text = null;
+                c = '+';
+                val = 0;
+                return;
+            }
+            else
+            {
+                if (Afisare.Text != null)
+                {
+                    if (c == '+')
+                        val = val + double.Parse(Afisare.Text);
+                    if (c == '-')
+                        val = val - double.Parse(Afisare.Text);
+                    if (c == '*')
+                        val = val * double.Parse(Afisare.Text);
+                    if (c == '/')
+                        val = val / double.Parse(Afisare.Text);
+                }
+                c = ' ';
+                Afisare.Text = val.ToString();
+            }
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            Afisare.Text = null;
+            c = '+';
+            val = 0;
+        }
+
+        private void Opusul_Click(object sender, EventArgs e)
+        {
+            double aux;
+            if (Afisare.Text != null)
+            {
+                aux = -double.Parse(Afisare.Text);
+                Afisare.Text = aux.ToString();
+            }
+        }
+    }
+}
